8e09433e0e3d1241045ec3a0c762a4c9cc3d6870
Ricardo Alvizar <alvizaribarraa@gmail.com>
2024-03-02 04:01:25 -0600

    reto terminado

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let textoEntrada = document.querySelector(".texto_entrada")
 let textoSalida = document.querySelector(".texto_encriptado")
+let botonCopiar = document.querySelector('#btncopiar')
 // array del vocabulario.
 let vocabulario = [
     ['e', 'enter'],
@@ -12,7 +13,20 @@ let vocabulario = [
 function bntencriptar() {
     let texto = textoNuevo(textoEntrada.value);    
     textoSalida.value= texto
-    
+    let display = texto.length;
+    if (display > 0) {
+        //objetos visibles cuando hay texto.
+        document.getElementById('btncopiar').style.display= 'flex';
+        document.querySelector('.texto_encriptado').style.display= 'block';
+        document.querySelector('.indicacion').style.display= 'none';
+        document.querySelector('.imagen_muñeco').style.display= 'none';
+    } else {
+        //objetos cambiados cuando no hay texto
+        document.getElementById('btncopiar').style.display= 'none';
+        document.querySelector('.texto_encriptado').style.display= 'none';
+        document.querySelector('.indicacion').style.display= 'block';
+        document.querySelector('.imagen_muñeco').style.display= 'block';
+    }
 }
 function textoNuevo(frasenueva) {
     for (let i = 0; i < vocabulario.length; i++) {
@@ -28,8 +42,9 @@ function textoNuevo(frasenueva) {
 // codigo btn desencriptar
 function btndesencriptar() {
     let desencriptar = desencriptarTexto(textoEntrada.value);
-   textoSalida.value = desencriptar
-}
+    textoSalida.value = desencriptar;
+    }
+
 function desencriptarTexto(textoDesencriptado) {
     for (let i = 0; i < vocabulario.length; i++) {
         if (textoDesencriptado.includes(vocabulario[i][1])) {
@@ -41,3 +56,8 @@ function desencriptarTexto(textoDesencriptado) {
     }
     return textoDesencriptado;
 }
+async function copy() {
+    let txt = textoSalida.value
+    await navigator.clipboard.writeText(txt)
+}
+botonCopiar.addEventListener('click',copy);
